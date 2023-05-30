4f9e21ad4b33b941cd37dfc9854263a5bf70f821
rcastillo <castillo.randolfo@gmail.com>
2023-05-30 19:26:50 -0300

    commit initial

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+export const SITE = {
+  title: "Intelligent systems",
+  description: "Computer-based systems that demonstrate intelligent behavior, including perception, reasoning, learning, and adaptation, to assist or replace human decision-making in complex and dynamic environments",
+  defaultLanguage: "en_US",
+  twitter: "@castillo9x",
+  github: "rcastillo9x",
+  linkedin: "randolfo-castillo-0451265",
+};
+
+export const OPEN_GRAPH = {
+  image: {
+    src: "",
+    alt: "",
+  },
+  twitter: "",
+};
+
+export const SIDEBAR = [
+  { text: "Agent theory", header: true },
+  { text: "Theory", link: "/agent/theory" },
+  { text: "Types", link: "/agent/types" },
+  { text: "Characteristics", link: "/agent/characteristics" },
+  { text: "Multiagent", link: "/agent/mas" },
+  { text: "Ubiquitous systems", link: "/agent/ubiquitous-systems" },
+  { text: "REAS", link: "/agent/reas" },
+  { text: "Types of environments", link: "/agent/enviroment" },
+  { text: "Types of programs", link: "/agent/programs-types" },
+  { text: "Reasoning", link: "/agent/reasoning" },
+  { text: "Types of architecture of intelligent agents", link: "/agent/types-of-aia" },
+
+  { text: "Information and knowledge.", header: true },
+  { text: "Data, Information ...", link: "/information/information" },
+  { text: "Types of data", link: "/information/types" },
+  { text: "Knowledge", link: "/information/knowledge" },
+  { text: "Data quality", link: "/information/data-quality" },
+
+  { text: "Patterns", header: true },
+  { text: "Introduction", link: "/patterns/introduction" },
+];
